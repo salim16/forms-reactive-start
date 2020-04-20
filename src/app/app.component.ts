542ffa866df25bea7271e2dbfdecd310e43806de
Mohd Salim <mohd.salim@soprasteria.com>
2020-04-20 22:10:57 +0530

    Reactive: Asynchronous Custom Validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,8 @@ export class AppComponent implements OnInit{
     this.signUpForm = new FormGroup({
       'userData' : new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email, 
+                 this.forbidenEmails])
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -49,5 +51,18 @@ export class AppComponent implements OnInit{
     }
     // It is important to return null, thats the way validators work
     return null;
+  }
+
+  forbidenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'test@test.com') {
+          resolve({'emailIsForbiden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
