98013e835132f97344fbf19b4dea5e7cbc7b45d3
Mohd Salim <mohd.salim@soprasteria.com>
2020-04-20 20:26:09 +0530

    Reactive: Creating custom validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,13 @@ export class AppComponent implements OnInit{
  
   genders = ['male', 'female'];
   signUpForm: FormGroup;
+  forbiddenUserNames = ['Chris', 'Anna'];
 
   // In reactive approach we create the form here in the typescript class
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       'userData' : new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -38,5 +39,15 @@ export class AppComponent implements OnInit{
 
   getControls() {
     return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
+
+  // Custom validator , {[s: string]: boolean} is the output type where [s: string] means any
+  // key which can transform into string
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    // It is important to return null, thats the way validators work
+    return null;
   }
 }
